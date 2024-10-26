2b6ea686780cf34d98caa0de8f42b05bf9483d9f
RadhePatel04 <radhep04@gmail.com>
2024-10-26 16:22:42 -0600

    cleaned up the entrantEventDetailsFragment imports

--- app/src/main/java/com/example/orange/ui/events/entrantEventDetailsFragment.java
+++ app/src/main/java/com/example/orange/ui/events/entrantEventDetailsFragment.java
@@ -1,20 +1,11 @@
 package com.example.orange.ui.events;
 
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Lifecycle;
-
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.example.orange.R;
 
